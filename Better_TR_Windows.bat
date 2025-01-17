3a3f51da90f908c7dc08c9209cacd56c0c6bdce9
ceeprus <cihanbeykoroglu@gmail.com>
2025-01-17 07:21:39 +0300

    fix Turkish language script typos

--- Better_TR_Windows.bat
+++ Better_TR_Windows.bat
@@ -1,17 +1,17 @@
 :: Balatro Turkish Translations
 ::
-:: Balatro iáin TR dil paketi kurulum scripti
-:: Äeviride yard’mda bulunmak iáin Discord (Balatro TR - Dil modu sunucumuza kat’labilirsiniz: https://discord.gg/dfy7b3zsVN 
-:: TÅm gÅncel kaynaklara buradan ula?abilirsiniz : https://github.com/ceeprus/balatro-turkish-translations
+:: Balatro icin TR dil paketi kurulum scripti
+:: Ceviride yardimda bulunmak icin Discord (Balatro TR - Dil modu sunucumuza katilabilirsiniz: https://discord.gg/dfy7b3zsVN 
+:: Tum guncel kaynaklara buradan ulagabilirsiniz : https://github.com/ceeprus/balatro-turkish-translations
 ::
-:: Bu dosya oyuna mod eklemek iáin Balamod kullanmaktad’r (https://github.com/balamod/balamod)
+:: Bu dosya oyuna mod eklemek icin Balamod kullanmaktadir (https://github.com/balamod/balamod)
 ::
 ::
 ::    ==================================
-::    ==  Kayboldun mu? Panik yapma!  ==
+::    ==  Kayboldun mug Panik yapma!  ==
 ::    ==================================
-::    Geri dînÅn ve sizi buraya getiren ba?lant’ya SA? TIKLAYIN, ard’ndan ?Ba?lant’y’ farkl’ kaydet...?.
-::    Ard’ndan kurulumu ba?latmak iáin indirilen dosyaya áift t’klay’n.
+::    Geri donun ve sizi buraya getiren baglantiya SAg TIKLAYIN, ardindan gBaglantiyi farkli kaydet...
+::    Ardindan kurulumu baglatmak icin indirilen dosyaya cift tiklayin.
 ::
 ::
 ::
@@ -25,32 +25,32 @@ set "resourcesFolder=Balatro_Localization_Resources"
 
 echo =========================================
 echo ==     Balatro Turkish Translations    ==
-echo ==     TR Dil paketinin yÅklenmesi     ==
-echo ==        Äeviriler ve resimler        ==
+echo ==     TR Dil paketinin yuklenmesi     ==
+echo ==        Ceviriler ve resimler        ==
 echo =========================================
 
-:: Kullan’c’ sorusu: TÅrkáe resimler kullan’lmal’ m’?
+:: Kullanici sorusu: Turkce resimler kullanilmali mig
 echo.
 echo.
-CHOICE /C YN /M "Gîrselleri TÅrkáe olarak m’ kullanmak istiyorsunuz?"
+CHOICE /C YN /M "Gorselleri Turkce olarak mi kullanmak istiyorsunuzg"
 if %errorlevel% equ 2 (
-    echo GîrÅntÅleri Ekleme
+    echo Goruntuleri Ekleme
     set "download_assets=false"
 ) else (
-    echo GîrÅntÅleri Ekle
+    echo Goruntuleri Ekle
     set "download_assets=true"
 )
 
-:: Varsay’lan Steam kurulumunu kontrol etme (libraryfolders.vdf Åzerinden)
+:: Varsayilan Steam kurulumunu kontrol etme (libraryfolders.vdf uzerinden)
 set "steamLibraryFile=C:\Program Files (x86)\Steam\steamapps\libraryfolders.vdf"
 
-:: E?er mevcut de?ilse, Balatro.exe dosyas’n’ elle seámek iáin gezgini aá’n
+:: Eger mevcut degilse, Balatro.exe dosyasini elle secmek icin gezgini acin
 if not exist "!steamLibraryFile!" (
     echo.
-    echo Oops, lÅtfen bize Balatro.exe dosyas’n’ nerede bulaca?’m’z’ gîsterin.
+    echo Oops, lutfen bize Balatro.exe dosyasini nerede bulacagimizi gosterin.
 
     set "balatroFile="
-    set "dialogTitle=balatro.exe seáiniz."
+    set "dialogTitle=balatro.exe seciniz."
     set "fileFilter=Balatro Executable (balatro.exe) | balatro.exe"
 
     for /f "delims=" %%I in ('powershell -Command "& { Add-Type -AssemblyName System.Windows.Forms; $dlg = New-Object System.Windows.Forms.OpenFileDialog; $dlg.Filter = '!fileFilter!'; $dlg.Title = '!dialogTitle!'; $dlg.ShowHelp = $true; $dlg.ShowDialog() | Out-Null; $dlg.FileName }"') do set "selectedFile=%%I"
@@ -59,21 +59,21 @@ if not exist "!steamLibraryFile!" (
         set "balatroFile=!selectedFile!"
         echo Balatro.exe : !balatroFile!
     ) else (
-        echo Balatro.exe : Dosyas’ seáili de?il. Kurulum iptal edildi.
+        echo Balatro.exe : Dosyasi secili degil. Kurulum iptal edildi.
         goto :end
     )
 )
 
-:: Geáici klasîrleri olu?turma
+:: Gecici klasorleri olugturma
 if not exist "%resourcesFolder%" mkdir "%resourcesFolder%"
 if not exist "%resourcesFolder%\resources\textures" mkdir "%resourcesFolder%\resources\textures"
 if not exist "%resourcesFolder%\resources\textures\1x" mkdir "%resourcesFolder%\resources\textures\1x"
 if not exist "%resourcesFolder%\resources\textures\2x" mkdir "%resourcesFolder%\resources\textures\2x"
 
-:: En son Balamod sÅrÅmÅnÅn ad’n’ alma
+:: En son Balamod surumunun adini alma
 for /f %%a in ('powershell -command "$tag = (Invoke-RestMethod -Uri 'https://api.github.com/repos/balamod/balamod/releases/latest' -TimeoutSec 10).tag_name; $tag"') do set latestTag=%%a
 
-:: Dosya adlar’n’n ve ba?lant’lar’n’n olu?turulmas’. Yaln’zca windows dosyas’ balamod-v.y.z-windows.exe olarak adland’r’ld’?’ sÅrece geáerlidir.
+:: Dosya adlarinin ve baglantilarinin olugturulmasi. Yalnizca windows dosyasi balamod-v.y.z-windows.exe olarak adlandirildigi surece gecerlidir.
 set "balamodFile=balamod-%latestTag%-windows.exe"
 set "balamodFileUrl=https://github.com/balamod/balamod/releases/download/%latestTag%/%balamodFile%"
 set "tr_repository=https://raw.githubusercontent.com/ceeprus/balatro-turkish-translations/main/localization_files"
@@ -101,7 +101,7 @@ set "tr_assets8BitDeck_opt22x=%tr_repository%/resources/textures/2x/8BitDeck_opt
 :: Balamod indir
 if not exist "%resourcesFolder%\%balamodFile%" (
     echo.
-    echo Balamod ?ndiriliyor...
+    echo Balamod gondiriliyor...
     echo.
     curl --ssl-no-revoke -L -o "%resourcesFolder%\%balamodFile%" %balamodFileUrl%
     echo.
@@ -144,12 +144,12 @@ echo.
 
 :: TR dil paketi enjeksiyonu
 echo.
-echo Dil paketi yÅkleniyor...
+echo Dil paketi yukleniyor...
 echo.
 
 
 if not defined balatroFile (
-    :: Steam varsay’lan olarak yÅklÅyse, Balamod'un Balatro dosyas’n’ aramas’na izin verin.
+    :: Steam varsayilan olarak yukluyse, Balamod'un Balatro dosyasini aramasina izin verin.
     "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\tr.lua -o localization/tr.lua
     "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\game.lua -o game.lua
     if "%download_assets%"=="true" (
@@ -173,7 +173,7 @@ if not defined balatroFile (
         "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\resources\textures\2x\8BitDeck_opt2.png -o resources/textures/2x/8BitDeck_opt2.png
     )
 ) else (
-    :: Aksi takdirde, daha înce seáilen Balatro.exe dosyas’n’ iáeren klasîrÅ gîndeririz.
+    :: Aksi takdirde, daha once secilen Balatro.exe dosyasini iceren klasoru gondeririz.
     for %%A in ("!balatroFile!") do set "balatroFolder=%%~dpA"
     "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\tr.lua -o localization/tr.lua
     "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\game.lua -o game.lua
@@ -203,9 +203,9 @@ echo %colorReset%
 echo.
 echo Dil paketi kuruldu !
 
-:: Kaynak dosyalar’n’ silme
+:: Kaynak dosyalarini silme
 if exist "%resourcesFolder%" rd /s /q "%resourcesFolder%"
-echo Balatro gÅncellendi !
+echo Balatro guncellendi !
 
 :end
 echo.
